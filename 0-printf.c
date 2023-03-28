fd9e4fb329eed7016015bec8f360395777620ce8
shadyEgypt <shadyrafat60@gmail.com>
2023-03-28 20:13:12 +0200

    task0

--- 0-printf.c
+++ 0-printf.c
@@ -1,40 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 
-
-/**
- * _printf - Custom implementation of printf function
- * @format: A string containing the format specifier and output string
- *
- * Return: Number of characters printed
- */
-int _printf0(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			count += handle_format_specifier(args, format);
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-		format++;
-	}
-
-	va_end(args);
-	return (count);
-}
-
-
 /**
  * handle_format_specifier - Handle format specifier character
  * @args: va_list of arguments for format specifier
@@ -50,6 +16,8 @@ int _printf0(const char *format, ...)
 		count += handle_char(args);
 	else if (*format == 's')
 		count += handle_string(args);
+	if (*format == 'd' || *format == 'i')
+		count += handle_integer(args);
 	else if (*format == '%')
 	{
 		_putchar('%');
--- 1-printf.c
+++ 1-printf.c
@@ -3,63 +3,6 @@
 
 
 /**
- * _printf - Custom implementation of printf function
- * @format: A string containing the format specifier and output string
- *
- * Return: Number of characters printed
- */
-int _printf1(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			count += handle_format_specifier(args, format);
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-			format++;
-	}
-
-	va_end(args);
-	return (count);
-}
-
-
-/**
- * handle_format_specifier - Handle format specifier character
- * @args: va_list of arguments for format specifier
- * @format: Pointer to format specifier character
- *
- * Return: Number of characters printed
- */
-int handle_format_specifier(va_list args, const char *format)
-{
-	int count = 0;
-
-	if (*format == 'd' || *format == 'i')
-		count += handle_integer(args);
-	else
-	{
-		_putchar('%');
-		_putchar(*format);
-		count += 2;
-	}
-
-	return (count);
-}
-
-
-/**
  * handle_integer - Handle integer format specifier
  * @args: va_list of arguments for format specifier
  *
